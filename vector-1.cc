fca57f8a99a23d775d75646e01ba4d15db86ce46
SIVARS <sivars@posteo.de>
2022-02-08 17:55:19 +0100

    update

---
+++ vector-1.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+
+int main () {
+
+    std::vector<int> einser_reihe {};
+    std::vector<int> divers_initialisierte_reihe {15,0,2127,-12,-199,30878,777,877,191,101};
+    std::vector<int> null_initialisierte_reihe(10);
+    std::vector<int> gleich_initialisierte_reihe(10,99);
+    std::vector<double> vector_double {};
+
+    for (int i {1}; i<100;i++) {
+        vector_int.push_back(i);
+    }
+
+    // alle daten ausgeben please
+    for (auto vector_int: size(vector_int)) {
+
+    }
+    std::cout << std::endl;
+
+    for (auto diverse_zahlen: size(divers_initialisierte_zahlen)) {
+        std::cout << i << "\t";
+    }
+    std::cout << std::endl;
+    for (auto gleich_initialisierte_zahlen){
+
+    }
+    // werte hinten anfügen
+    // werte löschen
+    // werte werte in der mitte einfügen
+
+}
